09980187b1c745257732e3cbe02343e4a978bfa7
xiaoyangLee <lixiaoyang2020@gmail.com>
2017-09-28 23:19:49 +0800

    Hello CS

---
+++ HelloWorld/HelloWorld.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/**
+ * namespace命名空间，这里我的理解是如同java的package 
+ */
+namespace HelloWorldApplication
+{   
+    //CS无需类名与文件名同名
+    class HelloWorldApp
+    {
+        static void Main(string[] args)
+        {   
+            //在命令行输出HelloWorld并换行
+            Console.WriteLine("Hello World");
+            //如果命令窗口一闪而过，那么久需要使用ReadKey()来保持窗口存在
+            //Console.ReadKey();
+        }
+    }
+}
