4e22ef740d903185c62c282239ae12b5b704e97d
xiaoyangLee <lixiaoyang2020@gmail.com>
2017-09-29 23:38:59 +0800

    add new files

---
+++ Rectangle/Rectangle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace RectangleApplication
+{   
+    //创建Rectangle矩形类
+    class Rectangle
+    {
+        //成员变量
+        double width;
+        double height;
+
+        public void GetValue()
+        {
+            Console.WriteLine("请输入长方形的宽:");
+            this.width = Console.Read();
+            Console.WriteLine("长方形的宽度是:{0}", width);
+            Console.WriteLine("请输入长方形的长:");
+            this.height = Console.Read();
+            Console.WriteLine("长方形的长度是:{0}",height);
+        }
+
+
+
+    }
+
+    class ExecuteRectangle
+    {
+         static void Main()
+        {
+            Rectangle rectangle = new Rectangle();
+            rectangle.GetValue();
+        }
+    }
+}
