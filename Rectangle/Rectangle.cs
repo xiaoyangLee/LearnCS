7d37bea42f49205364fed260e840f1162fd72c10
xiaoyangLee <lixiaoyang2020@gmail.com>
2017-09-30 22:08:58 +0800

    update file

--- Rectangle/Rectangle.cs
+++ Rectangle/Rectangle.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 
 namespace RectangleApplication
-{   
+{
     //创建Rectangle矩形类
     class Rectangle
     {
@@ -9,17 +9,38 @@ namespace RectangleApplication
         double width;
         double height;
 
+        //为成员变量赋值，读入用户从键盘输入的值
         public void GetValue()
         {
-            Console.WriteLine("请输入长方形的宽:");
-            this.width = Console.Read();
-            Console.WriteLine("长方形的宽度是:{0}", width);
-            Console.WriteLine("请输入长方形的长:");
-            this.height = Console.Read();
-            Console.WriteLine("长方形的长度是:{0}",height);
+            try
+            {
+                Console.WriteLine("请输入长方形的宽:");
+                //将ReadLine读取到的字符串转为double类型
+                this.width = double.Parse(Console.ReadLine());
+                Console.WriteLine("长方形的宽度是:{0}", width);
+                Console.WriteLine("请输入长方形的长:");
+
+                this.height = double.Parse(Console.ReadLine());
+                Console.WriteLine("长方形的长度是:{0}", height);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("请输入数字！");
+            }
+
         }
+        //计算面积
+        public double GetArea()
+        {
+            return width * height;
+        }
+        //显示结果
 
-
+        public void Display()
+        {
+            Console.WriteLine("此长方形的面积为：{0}", this.GetArea());
+        }
+         
 
     }
 
@@ -29,6 +50,8 @@ namespace RectangleApplication
         {
             Rectangle rectangle = new Rectangle();
             rectangle.GetValue();
+            rectangle.Display();
+           
         }
     }
 }
