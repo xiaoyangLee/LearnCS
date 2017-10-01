84e7370f198511c41ac9e1185fffdf425c9ba502
xiaoyangLee <lixiaoyang2020@gmail.com>
2017-10-01 23:34:32 +0800

    add new file

---
+++ TypeValue.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TypeValue
+{
+    class TypeValue
+    {
+        static void Main(string[] args)
+        {
+            //C#中的整形类型：byte short int long sbyte(8位有符号) ushort uint ulong 
+            byte a = 255; //byte类型值范围0~255
+            short b = 32767; //short类型-32768到32767
+            int c = 123456; //
+        }
+    }
+}
