38e71d11ef9bd114d248df4a929d82a0c23d9fe5
xiaoyangLee <lixiaoyang2020@gmail.com>
2017-11-18 16:53:59 +0800

    add new file

---
+++ HideEnumChange.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LearnCS
+{
+    //隐式枚举转换
+    class HideEnumChange
+    {
+        enum Days
+        {
+            Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday
+        }
+        /*
+         * 在C#中，隐式枚举转换允许把十进制整数0转换成任何枚举类型，而其他整数则不存在这种隐式转换
+         */
+       static void Main(String[] args)
+        {
+            Days currentDay;
+            //currentDay = 1; //如果赋值为1则会报错
+            currentDay = 0;
+            Console.WriteLine(currentDay);
+        }
+    }
+}
